917ba4d27b364c2e8f15e028c165cdc15a0f201b
Ricardo Feregrino <ricardoferegrinojr@gmail.com>
2024-04-18 00:30:12 -0600

    Removed things from airline

--- init.lua
+++ init.lua
@@ -2,28 +2,3 @@ require("fere")
 
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
--- set airline colors
-vim.cmd [[ let g:airline_theme='catppuccin' ]]
-
-vim.cmd [[ if !exists('g:airline_symbols')
-    let g:airline_symbols = {}
-  endif]]
-vim.cmd [[let g:airline#extensions#tabline#enabled = 1]]
-vim.cmd [[ let g:airline_left_sep = '⮀']]
-vim.cmd [[ let g:airline_left_alt_sep = '⮁' ]]
-vim.cmd [[ let g:airline_right_sep = '⮂' ]]
-vim.cmd [[ let g:airline_right_alt_sep = '⮃' ]]
-vim.cmd [[ let g:airline_symbols.branch = '⭠' ]]
-vim.cmd [[ let g:airline_symbols.readonly = '⭤' ]]
-vim.cmd [[ let g:airline_symbols.linenr = '⭡' ]]
-vim.cmd [[ let g:airline_left_sep = ''
-  let g:airline_left_alt_sep = ''
-  let g:airline_right_sep = ''
-  let g:airline_right_alt_sep = ''
-  let g:airline_symbols.branch = ''
-  let g:airline_symbols.colnr = ' ℅:'
-  let g:airline_symbols.readonly = ''
-  let g:airline_symbols.linenr = ' :'
-  let g:airline_symbols.maxlinenr = '☰ '
-  let g:airline_symbols.dirty='⚡' ]]
